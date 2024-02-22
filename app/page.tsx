2a04102342150234a9b283001ac9d24f0a830e80
Janaka Chamith <48000312+janaka99@users.noreply.github.com>
2024-02-22 23:37:12 +0530

    homepage update

--- app/page.tsx
+++ app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
           </div>
         </section>
         <section className="">
-          <LatestPost />
+          {/* <LatestPost /> */}
           <HomepagePosts />
         </section>
       </Container>
